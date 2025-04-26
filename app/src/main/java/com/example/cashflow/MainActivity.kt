7ec28d18476981277b0efb676cf04791f6367d05
Mateusz Stasiak <mateusz.stasiak16@gmail.com>
2025-04-26 03:09:09 +0200

    Add Basic Splash Screen

--- app/src/main/java/com/example/cashflow/MainActivity.kt
+++ app/src/main/java/com/example/cashflow/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,11 +12,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.cashflow.ui.theme.CashFlowTheme
+import com.example.cashflow.viewmodel.MainViewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                !viewModel.isReady.value
+            }
+        }
+
         enableEdgeToEdge()
         setContent {
             CashFlowTheme {
